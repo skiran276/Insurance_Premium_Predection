c2721e736c1105845cdd41761634a3c03f964cd1
shashi bhat <bhatshashi3@gmail.com>
2022-08-03 21:43:58 +0530

    message

--- insurance/pipeline/pipeline.py
+++ insurance/pipeline/pipeline.py
@@ -1,7 +1,8 @@
 from insurance.config.configuration import Configuration
-from insurance.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact
+from insurance.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact,DataTransformationArtifact
 from insurance.component.data_ingestion import DataIngestion
 from insurance.component.data_validation import DataValidation
+from insurance.component.data_transformation import DataTransformation
 from insurance.exception import InsuranceException
 from insurance.logger import logging, get_log_file_name
 import os,sys,uuid
@@ -35,11 +36,29 @@ class Pipeline():
         except Exception as e:
             raise InsuranceException(e, sys) from e
 
+
+    def start_data_transformation(self,
+                                  data_ingestion_artifact: DataIngestionArtifact,
+                                  data_validation_artifact: DataValidationArtifact
+                                  ) -> DataTransformationArtifact:
+        try:
+            data_transformation = DataTransformation(
+                data_transformation_config=self.config.get_data_transformation_pipeline_config(),
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_artifact=data_validation_artifact
+            )
+            return data_transformation.initiate_data_transformation()
+
+        except Exception as e:
+            raise InsuranceException(e, sys)
+
     def run_pipeline(self):
         try:
 
             data_ingestion_artifact=self.start_data_ingestion()
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
-
+            data_transformation_artifact = self.start_data_transformation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_artifact=data_validation_artifact)
         except Exception as e:
             raise InsuranceException(e,sys) from e
